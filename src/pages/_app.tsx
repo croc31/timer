1bfe37a285f68319a7ea2dfa9e73a45dca6b1bbc
croc_31 <clauberogerioo@gmail.com>
2022-05-18 16:23:45 -0300

    organizando documentos

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import { Layout } from "../components/Layout";
+import {SSRProvider} from '@react-aria/ssr';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <SSRProvider> 
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      </SSRProvider> 
+  );
+}
+
+export default MyApp;
